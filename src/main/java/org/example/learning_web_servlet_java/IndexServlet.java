0c42d2e0f53f7e493a6493d452f5a77af35408d0
Murad Mammadzada <92033053+MuradMT@users.noreply.github.com>
2025-06-18 16:53:12 +0100

    Servlets used before jsp,initial version of jsp,but hard to manage,rn we can use it only for handling logic.,how to create custom servlet.Override keyword. We have lifecycles,first we load servlet,then we call doInit for initialization,then handle requests with doGet,dopost etc,then we destroy.HTTP methods.request forwarding with request.getRequestDistpacher.forwarad(req,res) from servlet to jsp.request.setAttribute and request.getAttribute helps to get and SET ATTRIBUTES AND SEND FROM SERVLET TO JSP.We generally implement mvc with servlet-jsp-normal classes.

---
+++ src/main/java/org/example/learning_web_servlet_java/IndexServlet.java
@@ -0,0 +1,33 @@
+package org.example.learning_web_servlet_java;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "indexServlet", value = "/index")
+public class IndexServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        resp.getOutputStream().println("<h1>Hi</h1>");
+        req.setAttribute("message","Hello World!");
+        req.getAttribute("message");
+        req.getRequestDispatcher("index.jsp").forward(req,resp);
+        resp.sendRedirect("index.jsp");
+        req.getParameter("name");
+    }
+}
+//Notes
+/*
+    1.Servlets generally useful to apply all logic of jsp
+    They acts as controllers
+    Jsp should be a pure UI,no logic code here
+    Servlet needs to handle it
+    Before jsp we have pure servlets]but managing it hard
+    so jsp built on top of servlet
+    rn we can use servlet as logic management
+    2.override keyword works also here
+    3.mvc parts jsp-servlet
+ */
